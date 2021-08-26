1e0e5e6d845ecf05957dff9bf18347bf2ab12063
Alexandru Calmuc <calmuc.alexandru@cyber.local>
2021-08-26 17:00:01 +0300

    initial commit

---
+++ main.py
@@ -0,0 +1,32 @@
+from flask import Flask
+from flask_mail import Mail, Message
+
+MESSAGE = 'your message'
+SENDER = 'sender@example.com'
+RECIPIENT = 'recipient@example.com'
+BODY = 'message body'
+URL = '/'
+
+app = Flask(__name__)
+app.config['MAIL_SERVER']='smtp.mailtrap.io'
+app.config['MAIL_PORT'] = 2525
+app.config['MAIL_USERNAME'] = '7ba3199c5fff19'
+app.config['MAIL_PASSWORD'] = '19b108e3235d30'
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USE_SSL'] = False
+
+mail = Mail(app)
+
+
+@app.route(URL)
+def index():
+    msg = Message(MESSAGE,
+                  sender=SENDER,
+                  recipients=[RECIPIENT])
+    msg.body = BODY
+    mail.send(msg)
+    return 'Sent mail!'
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
